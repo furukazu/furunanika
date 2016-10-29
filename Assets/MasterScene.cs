c9b5343e7dd84cf12ec4b62749d00447fb281214
Furukazu Takahashi <yuki.ohiro@gmail.com>
2016-10-29 18:29:02 +0900

    init

---
+++ Assets/MasterScene.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MasterScene : MonoBehaviour {
+
+	public GameObject SingleCharPrefab;
+
+	private Dictionary<KeyCode,string> allKeys;
+
+	void Awake(){
+		allKeys = new Dictionary<KeyCode, string>();
+
+		allKeys[KeyCode.A] = "A";
+		allKeys[KeyCode.B] = "B";
+		allKeys[KeyCode.C] = "C";
+		allKeys[KeyCode.D] = "C";
+		allKeys[KeyCode.E] = "E";
+		allKeys[KeyCode.F] = "F";
+		allKeys[KeyCode.G] = "G";
+		allKeys[KeyCode.H] = "H";
+		allKeys[KeyCode.I] = "I";
+		allKeys[KeyCode.J] = "J";
+		allKeys[KeyCode.K] = "K";
+		allKeys[KeyCode.L] = "L";
+		allKeys[KeyCode.M] = "M";
+		allKeys[KeyCode.N] = "N";
+		allKeys[KeyCode.O] = "O";
+		allKeys[KeyCode.P] = "P";
+		allKeys[KeyCode.Q] = "Q";
+		allKeys[KeyCode.R] = "R";
+		allKeys[KeyCode.S] = "S";
+		allKeys[KeyCode.T] = "T";
+		allKeys[KeyCode.U] = "U";
+		allKeys[KeyCode.V] = "V";
+		allKeys[KeyCode.W] = "W";
+		allKeys[KeyCode.X] = "X";
+		allKeys[KeyCode.Y] = "Y";
+		allKeys[KeyCode.Z] = "Z";
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+	
+		foreach(var kv in allKeys){
+			if(Input.GetKeyDown(kv.Key)){
+				var newChar = GameObject.Instantiate(SingleCharPrefab);
+				newChar.transform.localPosition = Vector3.zero;
+				newChar.GetComponent<SingleChara>().SetText(kv.Value);
+			}
+		}
+
+
+
+	}
+}
---
+++ Assets/SingleChara.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SingleChara : MonoBehaviour {
+
+	public TextMesh Target;
+
+	private Vector3 speed;
+
+	void Awake(){
+		speed = Quaternion.Euler(0,0,Random.value*360) * (Vector3.up/64.0f);
+		print(speed);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var vp = Camera.main.WorldToViewportPoint(this.transform.position);
+		if(vp.x<0 || vp.y<0 || vp.x>1 || vp.y > 1){
+			Destroy(this.gameObject);
+			return;
+		}
+	}
+
+	void FixedUpdate(){
+		this.transform.Translate(speed);
+	}
+
+	public void SetText (string a)
+	{
+		Target.text = a;
+	}
+}
